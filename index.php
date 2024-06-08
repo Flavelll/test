2d8f480b9783dddcae6357840b5d1b53c21343e8
Flavelll <56677196+Flavelll@users.noreply.github.com>
2024-06-08 21:50:50 +0300

    Update-v index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,8 +8,18 @@
     <h1>Hello, PHP!</h1>
     <?php
         echo "<p>This is a simple PHP web page running in a Docker container.</p>";
+
+        // Current date and time
         $current_date = date('Y-m-d H:i:s');
         echo "<p>Current date and time is: $current_date</p>";
+
+        // Host IP address
+        $host_ip = gethostbyname(gethostname());
+        echo "<p>Host IP address is: $host_ip</p>";
+
+        // User IP address
+        $user_ip = $_SERVER['REMOTE_ADDR'];
+        echo "<p>User IP address is: $user_ip</p>";
     ?>
 </body>
 </html>
